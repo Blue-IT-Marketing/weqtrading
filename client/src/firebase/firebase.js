52bd68687525ca720f19b092aade9f13523bec9f
justice <mobiusndou@gmail.com>
2019-07-24 23:05:18 +0200

    registered weqtrading firebase.app

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 const config = {
-	apiKey: 'AIzaSyATnH54OeLn-T1c3D_clH2lo2FFz4IHRQA',
-	authDomain: 'p2ptraders-app.firebaseapp.com',
-	databaseURL: 'https://p2ptraders-app.firebaseio.com',
-	projectId: 'p2ptraders-app',
-	storageBucket: 'p2ptraders-app.appspot.com',
-	messagingSenderId: '339827645019'
+	apiKey: "AIzaSyBRJIk9O1Y8CAh9pQ9tKy9hPkDHnI4hjEc",
+	authDomain: "weqtrading.firebaseapp.com",
+	databaseURL: "https://weqtrading.firebaseio.com",
+	projectId: "weqtrading",
+	storageBucket: "weqtrading.appspot.com",
+	messagingSenderId: "308547563709",
+	appId: "1:308547563709:web:def203ccd0d35c07"
 
 };
 
@@ -17,11 +18,7 @@ try{
 	console.log('firebase app already configured');
 }
 
-
 const auth = firebase.auth();
-
-
-
 
 export {auth,
 	firebase
