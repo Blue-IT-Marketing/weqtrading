718e762e1ee3f5b1ae51af7604e56458ea9dfd59
justice <mobiusndou@gmail.com>
2019-07-25 19:25:30 +0200

    init

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,6 +1,6 @@
 
 import * as program_constants from './program_constants';
 import * as routes from './routes';
-import {Utils} from './utilities';
 
-export { program_constants,routes,Utils};
+
+export { program_constants,routes};
--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -2,7 +2,7 @@
 export const signup_page = '/signup';
 export const signin = '/signin';
 export const signout = '/signout';
-export const landing = '/';
+
 export const home_page = '/';
 
 export const admin = '/admin';
@@ -15,12 +15,8 @@ export const login_page = '/login';
 export const profile_page = '/profile';
 export const chat = '/chat';
 
-export const employees = '/employees';
 export const contact_details = '/contact/details';
 export const contact_form = '/contact/form';
-
-export const public_profile = '/profile/public/';
-export const private_profile = '/profile/private/';
 
 
 
