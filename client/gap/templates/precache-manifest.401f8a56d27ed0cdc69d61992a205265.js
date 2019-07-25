612ceabeb115c7e3a40391b9db8742f09663ed5e
justice <mobiusndou@gmail.com>
2019-07-25 19:37:16 +0200

    gcp routing issues
    fixing gcp routing issues

---
+++ client/gap/templates/precache-manifest.401f8a56d27ed0cdc69d61992a205265.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342dc9fc772051a579c577f178fb0269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53605b8c4d7d5220406",
+    "url": "/static/css/main.7d98405e.chunk.css"
+  },
+  {
+    "revision": "4bd5581e95478104aea0",
+    "url": "/static/js/2.0be90309.chunk.js"
+  },
+  {
+    "revision": "e53605b8c4d7d5220406",
+    "url": "/static/js/main.35c394da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
